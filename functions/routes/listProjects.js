63ad2e2ef2f10ef7df9266732b42497745685a48
arvin <arvinhow95@gmail.com>
2023-11-19 12:54:34 +0800

    fixed missing import

--- functions/routes/listProjects.js
+++ functions/routes/listProjects.js
@@ -1,6 +1,7 @@
+import { supabase } from './lib/helper.js';
+
 export function listProjects(fastify, opts, done) {
 	fastify.get('/list_projects', async (req, res) => {
-		//check if body contain information
 		const { data } = await supabase.from('projects').select();
 
 		res.code(200).send(data);
--- functions/routes/syncProject.js
+++ functions/routes/syncProject.js
@@ -1,12 +1,12 @@
-import { v4 as uuid } from 'uuid'
-import { calcChecksum, sendError, supabase } from './lib/helper.js'
+import { v4 as uuid } from 'uuid';
+import { calcChecksum, sendError, supabase } from './lib/helper.js';
 
 export function syncProject(fastify, opts, done) {
 	fastify.post('/sync_project', async (req, res) => {
-		const { project_uuid, model_data } = req.body
+		const { project_uuid, model_data } = req.body;
 
-		if (!project_uuid) return sendError(res, 'Missing project_uuid')
-		if (!model_data) return sendError(res, 'Missing data')
+		if (!project_uuid) return sendError(res, 'Missing project_uuid');
+		if (!model_data) return sendError(res, 'Missing data');
 
 		//check if project exist
 		const { data: projectExist } = await supabase
@@ -14,40 +14,40 @@ export function syncProject(fastify, opts, done) {
 			.select()
 			.eq('project_uuid', project_uuid)
 			.limit(1)
-			.single()
+			.single();
 
-		if (!projectExist) return sendError(res, 'project_uuid does not exist')
+		if (!projectExist) return sendError(res, 'project_uuid does not exist');
 
 		// upsert
 		const { data, error } = await supabase.from('project_model_data').upsert({
 			project_uuid,
 			model_data,
-		})
+		});
 
-		if (error) return sendError(res, error)
+		if (error) return sendError(res, error);
 
 		res.send({
 			status: 'success',
 			message: 'Project model_data uploaded to database',
-		})
-	})
+		});
+	});
 
 	fastify.get('/sync_project', async (req, res) => {
-		const { project_uuid } = req.query
+		const { project_uuid } = req.query;
 
-		if (!project_uuid) return sendError(res, 'Require project_uuid')
+		if (!project_uuid) return sendError(res, 'Require project_uuid');
 
 		const { data, error } = await supabase
 			.from('project_model_data')
 			.select()
 			.eq('project_uuid', project_uuid)
 			.limit(1)
-			.single()
+			.single();
 
-		if (error) return sendError(res, error)
+		if (error) return sendError(res, error);
 
-		res.send(data)
-	})
+		res.send(data);
+	});
 
-	done()
+	done();
 }
